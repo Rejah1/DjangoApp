c9be8ed8cc85cc6970ed14246165a583cc95eeb2
Rejah <raji_97@live.co.uk>
2017-11-02 04:11:52 +0000

    Update 2

--- testing/logbook/views.py
+++ testing/logbook/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from logbook.forms import RegistrationForm
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 
@@ -15,12 +15,12 @@ def home(request):
 
 def register(request):
     if request.method =='POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/logbook')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
 
         args = {'form': form}
         return render(request, 'logbook/register.html', args)
