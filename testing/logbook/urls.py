a57c23222ede012da1bcce2b321d872ac792fb19
Rejah <raji_97@live.co.uk>
2017-11-02 02:51:00 +0000

    Update Fixed

--- testing/logbook/urls.py
+++ testing/logbook/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .import views
+from . import views
 from django.contrib.auth.views import login, logout
 
 urlpatterns = [
--- testing/logbook/views.py
+++ testing/logbook/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-# render, renders the actual HTML template
-# Create your views here.
-def home(request):
-	# return HttpResponse('Home Page!')
-	
-	numbers = [1,2,3,4]
-	name = 'Rejah'
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
-	args = {'myName' : name, 'numbers': numbers}
-	return render(request, 'logbook/home.html', args)
+def home(request):
+    numbers = [1,2,3,4,5]
+    name = 'Rejah'
+
+    args = {'myName': name, 'numbers' : numbers }
+    return render(request,'logbook/home.html', args)
+
+
 
 def register(request):
-	if request.method =='POST':
-		form == UserCreationForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('/logbook')
-	else:
-		form = UserCreationForm()
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/logbook')
+    else:
+        form = UserCreationForm()
 
-		args = {'form': form}
-		return render(request, 'logbook/register.html', args)
+        args = {'form': form}
+        return render(request, 'logbook/register.html', args)
